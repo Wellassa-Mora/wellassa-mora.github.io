dbde2c34a79d1783b360179205b53c7903ee2579
Suhas Dissanayake <suhasdissa@gmail.com>
2025-01-23 22:08:56 +0530

    Update site metadata cover image

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Wellassa Mora';
-export const SITE_DESCRIPTION = 'Badulla District Engineering Students\' Forum of University of Moratuwa';
+export const SITE_TITLE = 'Wellassa Mora - Badulla District Engineering Students\' Forum of University of Moratuwa';
+export const SITE_DESCRIPTION = 'WellassaMora - is a society driven by the undergraduate and graduate students who entered the University of Moratuwa from Badulla district.';
